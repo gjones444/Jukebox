f46c6378221adc79aa241a9478580b36bdccbb7b
G.Funk <G.Funk@Gwhiz-Mac.home>
2017-11-04 17:09:57 -0400

    Done

---
+++ js/script.js
@@ -0,0 +1,55 @@
+var playlist = document.querySelector('ul.tracklist');
+var track = document.getElementsByTagName('li').length;
+
+playlist.addEventListener('click', function(e){
+	e.preventDefault();
+	/* console.log(e); */
+
+	var songName = e.target.getAttribute('data-src');
+	for (var i = 0; i < track; i++) {
+		if (document.getElementsByTagName('li')[i].getAttribute('data-src')===songName){
+			break;
+		}
+	};
+
+	var Jukebox = document.querySelector('#player')
+
+	if( Jukebox ){
+		if(songName===Jukebox.getAttribute('src')){
+			if(Jukebox.paused){
+			Jukebox.play();
+			e.target.id = 'playing';
+
+		} else  {
+			Jukebox.pause();
+			e.target.id = 'paused';
+		}
+
+	}	else{
+			Jukebox.src = songName;
+			Jukebox.play();
+			if(document.querySelector('#playing')){
+				document.querySelector('#playing').id = '';
+			}
+		}
+	}
+		else {
+		var Jukebox = document.createElement('audio');
+		Jukebox.id = 'player';
+		e.target.id = 'playing';
+		Jukebox.src = songName;
+		document.body.appendChild(Jukebox);
+		Jukebox.play();
+
+				}
+});
+
+document.getElementById("add").onclick = function() {
+    var text = document.getElementById("upload").value;
+		var songs = document.getElementById("list")
+
+		text = text.split('\\');
+		text = text.pop();
+
+    document.getElementById("list").append(text);
+}
